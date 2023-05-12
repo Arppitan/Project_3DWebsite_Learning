b9e1a5e260632aa3502f3e04a718f711d3df7a09
Arppitan <a47.arppitan@gmail.com>
2023-05-12 21:14:48 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
+
+function App() {
+  return(
+  <main className='app transition-all ease-in'>
+    <Home />
+    <Canvas />
+    <Customizer />
+
+    <h1 className="head-text"></h1>
+  </main>
+  
+  )
+}
+
+export default App
